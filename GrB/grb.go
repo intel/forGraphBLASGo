e806e37750ed39f2fe3af79c6958387728ac3c23
Pascal Costanza <pascal.costanza@intel.com>
2023-09-18 10:46:52 +0200

    Added a note about the examples not running in the Go Playground.

--- GrB/grb.go
+++ GrB/grb.go
@@ -1,5 +1,8 @@
 /*
 Package GrB provides a binding for GraphBLAS, and more specifically for the SuiteSparse:GraphBLAS implementation of the
 GraphBLAS specification, for the Go programming language.
+
+The examples shown below currently do not run in the Go Playground. However, they do run when you copy them locally,
+or when you perform a "go test" from the GrB package.
 */
 package GrB
